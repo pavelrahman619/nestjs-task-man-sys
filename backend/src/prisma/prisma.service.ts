9ec392344d2e34c8d3327ca8895fc90d3476ec62
pavelrahman619 <pavelrahman.dev@gmail.com>
2025-07-26 23:11:00 +0600

    add prisma module

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
-
+import { PrismaClient } from '../../generated/prisma/index';
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  constructor() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://postgres:123@localhost:5434/nest',
+        },
+      },
+    });
+  }
+}
